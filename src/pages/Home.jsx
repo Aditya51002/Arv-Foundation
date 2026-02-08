61b11421492436acb03ffa4f46524b2bd053640c
Aditya51002 <adityamaurya510@gmail.com>
2026-02-08 23:07:29 +0530

    add admin drive publishing with home page announcement popup

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import Hero from "../components/Hero.jsx";
 import StorySection from "../components/StorySection.jsx";
+import DriveAnnouncementPopup from "../components/DriveAnnouncementPopup.jsx";
 
 const Home = () => {
   return (
     <div className="pb-14 space-y-12">
+      <DriveAnnouncementPopup />
       <Hero />
       <StorySection />
     </div>
