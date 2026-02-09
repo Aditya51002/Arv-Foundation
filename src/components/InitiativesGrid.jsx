b41f3422149f99e4b1665b6b737dd9367a7f12de
16Nidhi <nidhi12304741@gmail.com>
2026-02-09 22:05:07 +0530

    some changes made

--- src/components/InitiativesGrid.jsx
+++ src/components/InitiativesGrid.jsx
@@ -3,7 +3,6 @@ import { Link } from "react-router-dom";
 import { useLanguage } from "../context/LanguageContext.jsx";
 import initiativesContent from "../data/initiativesContent.js";
 import { Sparkles, Recycle, Droplet, ArrowRight } from "lucide-react";
-import { useLanguage } from "../context/LanguageContext.jsx";
 // import { Sparkles, Recycle, Droplet, ArrowRight } from "lucide-react";
 import initiativeImage1 from "../Images/Initiative/initiativeimage1.jpg";
 import initiativeImage2 from "../Images/Initiative/initiativeimage2.jpg";
