5986d973aae168651ae43517c56ddc68cae9287b
Aditya51002 <adityamaurya510@gmail.com>
2026-02-10 22:56:38 +0530

    Added recent and past drive to be shown in the home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,15 @@
 import Hero from "../components/Hero.jsx";
 import StorySection from "../components/StorySection.jsx";
-import DriveAnnouncementPopup from "../components/DriveAnnouncementPopup.jsx";
+import OngoingDrives from "../components/OngoingDrives.jsx";
 // Parallax moved into StorySection to avoid transforming the heading
 // (wrapping the whole StorySection caused the heading to be translated under the fixed navbar)
-import Timeline from "../components/Timeline.jsx";
 
 const Home = () => {
   return (
     <div className="pb-14 space-y-12">
-      <DriveAnnouncementPopup />
       <Hero />
       <StorySection />
-      <Timeline />
+      <OngoingDrives />
     </div>
   );
 };
